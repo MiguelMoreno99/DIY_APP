2ad139eb8f25286905b6fe169fe2bd8c6620f1b0
Miguel Moreno <mmd.-@hotmail.com>
2024-10-31 22:36:46 -0600

    SE IMPLEMENTA LA VENTANA DE NEW PUBLICATION

--- app/src/main/java/com/example/diyapp/ui/newpublication/NewPublicationFragment.kt
+++ app/src/main/java/com/example/diyapp/ui/newpublication/NewPublicationFragment.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.diyapp.R
+import com.example.diyapp.data.adapter.favorites.feedFavoritesAdapter
+import com.example.diyapp.data.adapter.favorites.feedFavoritesProvider
 import com.example.diyapp.databinding.FragmentNewPublicationBinding
 
 class NewPublicationFragment : Fragment() {
@@ -17,5 +24,19 @@ class NewPublicationFragment : Fragment() {
     ): View {
         _binding = FragmentNewPublicationBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Opciones para el Spinner
+        val options = listOf("Nature", "Wood", "Garden")
+        val spinner: Spinner = view.findViewById(R.id.spinnerOptions)
+
+        // Adaptador para el Spinner
+        val adapter = ArrayAdapter(view.context,android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
     }
 }
